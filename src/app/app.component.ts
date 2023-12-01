4cba722e41a0ff328d91d5fd6a79854c76392c75
Nishanttrivedii <nishanttrivedi103@gmail.com>
2023-12-01 12:50:49 +0530

    newCommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,42 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import { Todo } from './todo';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'AngularApp';
-}
+  title:string = 'AngularApp';
+ todoValue:string="";
+ list:Todo[]=[];
+
+ ngOnInit(){
+  this.list=[];
+  this.todoValue="";
+ }
+
+ addItem(){
+  if(this.todoValue!="")
+  {
+    const newItem:Todo={
+      id:Date.now(),
+      value:this.todoValue,
+      isDone:false
+    };
+    this.list.push(newItem);
+  }
+  this.todoValue="";
+ }
+
+ 
+
+ deleteItem(id:number){
+  this.list=this.list.filter(item=>item.id!=id)
+ }
+};
+
